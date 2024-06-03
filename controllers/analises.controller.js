ea1b4cf4ac72ba03131bc8d955d29fe17730a695
ruiiramos73 <ruiramos2004@gmail.com>
2024-06-04 00:55:27 +0100

    Add update functionality

--- controllers/analises.controller.js
+++ controllers/analises.controller.js
@@ -94,3 +94,42 @@ exports.create = async (req, res) => {
         });
     }
 };
+
+exports.update = async (req, res) => {
+    let analise;
+    try {
+        analise = await Analise.findByPk(req.params.id);
+
+        if (!analise) {
+            return res.status(404).json({
+                success: false, msg: `Análise with ID ${req.params.id} not found.`
+            });
+        }
+
+        let affectedRows = await analise.update(req.body);
+
+        if(affectedRows[0] === 0){
+            return res.status(200).json({
+                success: true, 
+                msg: `No updates were made to análise with ID ${req.params.id}.`
+            });
+        }
+
+        return res.json({
+            success: true,
+            msg: `Análise with ID ${req.params.id} was updated successfully.`
+        });
+    }
+    catch (err) {
+        if (err instanceof ValidationError)
+            return res.status(400).json({ 
+                success: false, 
+                msg: err.errors.map(e => e.message) 
+            });
+
+        res.status(500).json({
+            success: false, 
+            msg: `Error retrieving análise with ID ${req.params.id}.`
+        });
+    };
+};
